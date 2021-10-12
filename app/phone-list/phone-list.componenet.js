2abe0678390fcd3a0ebd19bddd82cdcd67cfa423
Vurtuga <61892642+MohamedKahlifa@users.noreply.github.com>
2021-10-13 00:41:37 +0200

    Update

--- app/phone-list/phone-list.componenet.js
+++ app/phone-list/phone-list.componenet.js
@@ -1,17 +1,32 @@
 angular.module('phoneList').component('phoneList', {
   templateUrl: 'app/phone-list/phone-list.template.html',
-  controller: function PhoneListController() {
-      this.phones = [
-        {
-          name: 'Nexus S',
-          snippet: 'Fast just got faster with Nexus S.'
-        }, {
-          name: 'Motorola XOOM™ with Wi-Fi',
-          snippet: 'The Next, Next Generation tablet.'
-        }, {
-          name: 'MOTOROLA XOOM™',
-          snippet: 'The Next, Next Generation tablet.'
-        }
-      ];
-    }
+  controller: ['$http',PhoneListController]
+});
+
+function PhoneListController($http) {
+  let self = this;
+  $http.get('app/Data/phones.json').then(function(response){
+    self.phones = response.data;
   });
+  self.orderProp="age";
+}
+
+
+
+    /*------ Static Data --------- 
+    this.phones = [
+      {
+        name: 'Nexus S',
+        snippet: 'Fast just got faster with Nexus S.',
+        age: 1
+      }, {
+        name: 'Motorola XOOM™ with Wi-Fi',
+        snippet: 'The Next, Next Generation tablet.',
+        age: 2
+      }, {
+        name: 'MOTOROLA XOOM™',
+        snippet: 'The Next, Next Generation tablet.',
+        age: 3
+      }
+    ]; 
+    --------------------------------*/
